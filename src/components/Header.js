1e58eb058cd11988c683ac1ece16df90eb3c6736
I527245 <rajeev.kumar13@sap.com>
2024-03-07 00:31:32 +0530

    fixing bugs

--- src/components/Header.js
+++ src/components/Header.js
@@ -46,7 +46,7 @@ const Header = () => {
       }
     });
     // This will be called when component unmounts
-    return ( () => unSubscribe())
+    // return ( () => unSubscribe())
   }, []);
   return (
     <div className="absolute flex justify-between w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
