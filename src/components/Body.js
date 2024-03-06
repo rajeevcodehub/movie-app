b50da5c635923a9ad100e39b845d29fbfe8818ba
I527245 <rajeev.kumar13@sap.com>
2024-03-06 22:20:11 +0530

    fixing bugs

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,20 +1,10 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  useNavigate,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
-import { Provider, useDispatch } from "react-redux";
-import appStore from "../utils/appStore";
-import { useEffect } from "react";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase";
-import { addUser, removeUser } from "../utils/userSlice";
+import { useDispatch } from "react-redux";
 import Browse from "./Browse";
 
 const Body = () => {
-  const dispatch = useDispatch();
   const router = createBrowserRouter([
     {
       path: "/",
@@ -30,28 +20,7 @@ const Body = () => {
     },
   ]);
 
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/auth.user
-        const { uid, email, displayName } = user;
-        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
-        
-        // ...
-      } else {
-        dispatch(removeUser());
-        // User is signed out
-        // ...
-      }
-    });
-  }, []);
-
-  return (
-    
-      <RouterProvider router={router} />
-  
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default Body;
