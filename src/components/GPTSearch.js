977754997cc83bcb9efa7723518fa193de97b6a8
I527245 <rajeev.kumar13@sap.com>
2024-03-18 23:32:02 +0530

    added language change

--- src/components/GPTSearch.js
+++ src/components/GPTSearch.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { NetflixBackgroungImage } from "../utils/constant";
 import Search from "./Search";
+import Header from "./Header";
+import { useDispatch } from "react-redux";
+import { changeLanguage } from "../utils/configSlice";
 const GPTSearch = () => {
+  const dispatch = useDispatch();
+ const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  }
   return (
     <div>
-      <Search />
-      <img src={NetflixBackgroungImage} alt="image" />
+      <div className="absolute flex right-0 rounded-md p-5 ">
+        <select className="rounded-sm" onChange={handleLanguageChange}>
+          <option value="en">English</option>
+          <option value="hindi">Hindi</option>
+          <option value="german">German</option>
+        </select>
+      </div>
+      <div>
+        <Search />
+        <img src={NetflixBackgroungImage} alt="image" />
+      </div>
     </div>
   );
 };
